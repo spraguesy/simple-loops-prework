48f22d162e09ef9edd7ce7be8235cac9362f0fc3
Andrew Sprague <andrewsprague@outlook.com>
2018-08-11 23:14:09 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -47,7 +47,7 @@ def until_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   counter = 0
   until counter == number_of_times do
-    puts number_of_times
+    puts phrase
     counter += 1
   end
 end
@@ -58,7 +58,7 @@ def for_iterator(number_of_times)
   
   phrase = "Welcome to Flatiron School's Web Development Course!"
   for counter in 1..number_of_times do 
-    puts number_of_times
+    puts phrase
   end
   
   
